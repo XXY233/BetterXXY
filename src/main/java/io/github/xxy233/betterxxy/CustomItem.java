5f82fd5a4e6d456fef9417592e81c4a9651bb339
TheLittle_Yang <2570984321@qq.com>
2024-04-26 15:16:48 +0800

    Initial commit

---
+++ src/main/java/io/github/xxy233/betterxxy/CustomItem.java
@@ -0,0 +1,21 @@
+package io.github.xxy233.betterxxy;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class CustomItem extends Item {
+    public CustomItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0f, 1.0f);
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+}
---
+++ src/main/java/io/github/xxy233/betterxxy/client/BetterXXYClient.java
@@ -0,0 +1,10 @@
+package io.github.xxy233.betterxxy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterXXYClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
