5470105d91da85f4a1272e85db81241b9fd3148c
lushangkan <1149014559@qq.com>
2024-04-29 20:37:38 +0800

    feat: Implement function
    Implement function.

---
+++ src/main/java/io/github/xxy233/rapidglassmining/mixin/MiningToolItemMixin.java
@@ -0,0 +1,24 @@
+package io.github.xxy233.rapidglassmining.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MiningToolItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MiningToolItem.class)
+public class MiningToolItemMixin {
+
+    @Inject(method = "getMiningSpeedMultiplier(Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;)F", at = @At("HEAD"), cancellable = true)
+    public void onGetMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
+        MiningToolItem miningToolItem = (MiningToolItem)(Object)this;
+        if (miningToolItem instanceof HoeItem && Registries.BLOCK.getId(state.getBlock()).getPath().equals("glass")) {
+            cir.setReturnValue(9999F);
+        }
+    }
+}
