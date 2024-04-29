ded0dee4e09119808ed91acdc3ea83eb5487a518
lushangkan <1149014559@qq.com>
2024-04-29 21:00:16 +0800

    feat: Add initialization log
    Add initialization log.

--- src/main/kotlin/io/github/xxy233/rapidglassmining/RapidGlassMining.kt
+++ src/main/kotlin/io/github/xxy233/rapidglassmining/RapidGlassMining.kt
@@ -1,9 +1,13 @@
 package io.github.xxy233.rapidglassmining
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import net.fabricmc.api.ModInitializer
 
-object RapidGlassMining: ModInitializer {
-    override fun onInitialize() {
 
+object RapidGlassMining: ModInitializer {
+    private val logger = KotlinLogging.logger {}
+
+    override fun onInitialize() {
+        logger.info { "RapidGlassMining initialized!" }
     }
 }
