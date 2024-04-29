834e08b08941af3ac0647a7610dfebcc36f865be
lushangkan <1149014559@qq.com>
2024-04-29 21:01:47 +0800

    style: Remove redundant import
    Remove redundant import.

--- src/main/java/io/github/xxy233/rapidglassmining/mixin/MiningToolItemMixin.java
+++ src/main/java/io/github/xxy233/rapidglassmining/mixin/MiningToolItemMixin.java
@@ -5,8 +5,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,8 +16,10 @@ public class MiningToolItemMixin {
     @Inject(method = "getMiningSpeedMultiplier(Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;)F", at = @At("HEAD"), cancellable = true)
     public void onGetMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
         MiningToolItem miningToolItem = (MiningToolItem)(Object)this;
+
         if (miningToolItem instanceof HoeItem && state.getBlock() instanceof AbstractGlassBlock) {
             cir.setReturnValue(9999F);
         }
     }
+
 }
